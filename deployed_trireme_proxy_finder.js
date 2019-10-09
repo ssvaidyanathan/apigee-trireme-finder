90a5a2d6743d0257ba13734204e5ad2c6c0fb7a3
Sai Saran Vaidyanathan <ssvaidyanathan@google.com>
2019-10-09 10:44:47 -0700

    Finder for deployed proxies

---
+++ deployed_trireme_proxy_finder.js
@@ -0,0 +1,181 @@
+const colors = require("colors/safe");
+const inquirer = require("inquirer");
+const rp = require("request-promise");
+const util = require("util");
+const Spinner = require('cli-spinner').Spinner;
+
+let questions = [
+	{
+    type: 'input',
+    name: 'host',
+    default: "api.enterprise.apigee.com",
+    message: colors.yellow("Please provide the Apigee Management Host: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red("Apigee Management Host name cannot be empty!");
+      return true;
+    }
+   },
+   {
+    type: 'input',
+    name: 'org',
+    message: colors.yellow("Please provide the Apigee Edge Organization name: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red("Apigee Edge Organization name cannot be empty!");
+      return true;
+    }
+   },
+   {
+    type: 'input',
+    name: 'env',
+    message: colors.yellow("Please provide the Apigee Edge Environment name: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red("Apigee Edge Environment name cannot be empty!");
+      return true;
+    }
+   },
+   {
+	  type: 'list',
+	  name: 'authType',
+	  message: colors.yellow("Which Auth type would you like to use?"),
+	  choices: ['Basic', 'OAuth'],
+	  validate: function(value) {
+      if(!value)
+        return colors.red("Please select an Auth type!");
+      return true;
+    }
+   }
+];
+
+
+let basicAuthPrompt = [
+{
+    type: 'input',
+    name: 'username',
+    message: colors.yellow("Please provide the Apigee Edge username: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red('Apigee Edge username cannot be empty!');
+      return true;
+    }
+   },
+   {
+    type: 'password',
+    name: 'password',
+    mask: '*',
+    message: colors.yellow("Please provide the Apigee Edge password: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red('Apigee Edge password cannot be empty!')
+      return true;
+     } 
+    }
+];
+
+let oAuthPrompt = [
+{
+    type: 'input',
+    name: 'oauthToken',
+    message: colors.yellow("Please provide the Apigee OAuth token: "),
+    validate: function(value) {
+      if(!value)
+        return colors.red('Apigee OAuth token cannot be empty!');
+      return true;
+    }
+   }
+];
+
+inquirer.prompt(questions).then(async function(options) {
+  if(options.authType === "Basic"){
+  	inquirer.prompt(basicAuthPrompt).then(async function(authOptions) {
+  		await getTriremeProxies(options, authOptions);
+  	});
+  }else {
+  	inquirer.prompt(oAuthPrompt).then(async function(authOptions) {
+  		await getTriremeProxies(options, authOptions);
+  	});
+  }
+});
+
+async function getTriremeProxies(config, auth){
+	let triremeProxies = [];
+	let spinner = new Spinner('Fetching all deployed proxies in '+config.org+ ' org in the '+ config.env +' env... %s');
+	spinner.start();
+	let apis = await getEntities(config, auth, "environments/"+config.env+"/deployments");
+	//let apis = await getEntities(config, auth, "apis");
+	if (apis.aPIProxy!=null && apis.aPIProxy.length==0) {
+		console.log("API Proxies Deployed: NONE");
+		return;
+	}
+	spinner.stop();
+	console.log("");
+	console.log("Fetch complete");
+	//console.log("Checking each proxy revision...");
+	spinner = new Spinner('Checking each proxy revision... %s');
+	console.log("");
+	spinner.start();
+	for(api of apis.aPIProxy){
+		//console.log("API: "+api.name + " Revision: "+api.revision[0].name);
+		let revisionMetaData = await getEntities(config, auth, "apis/"+api.name+"/revisions/"+api.revision[0].name);
+		if(revisionMetaData.resources !== null && revisionMetaData.resources.length > 0){
+			for (var i = 0; i < revisionMetaData.resources.length; i++){
+				if(revisionMetaData.resources[i].startsWith("node://") && revisionMetaData.resources[i].endsWith(".js")){
+					//revArr.push(api.revision[0].name);
+					triremeProxies.push({
+						"api": api.name,
+						"revision": api.revision[0].name
+					});	
+					break;
+				}
+			}
+		}
+	}
+	spinner.stop();
+	console.log("\n======== Trireme Proxies =========");
+	if (triremeProxies === null || triremeProxies.length === 0) {
+		console.log("NONE");
+		console.log ("==================================");
+		return;
+	}
+	for (proxy of triremeProxies){
+		console.log("API: "+ proxy.api);
+		console.log("Revision: "+ proxy.revision);
+		console.log("Environment: "+ config.env);
+		console.log("==================================");
+	}
+}
+
+async function getEntities(config, authConfig, entity){
+	//safeLog("Fetching "+entity+" from Apigee org: "+config.org);
+	let auth = "";
+	if(config.authType === "Basic")
+		auth = "Basic "+Buffer.from(authConfig.username+":"+authConfig.password).toString('base64');
+	else
+		auth = "Bearer "+ authConfig.oauthToken
+	let options = {
+	    method: "GET",
+	    uri: "https://"+config.host+"/v1/organizations/"+config.org+"/"+entity,
+	    headers: {
+        	"Authorization": auth
+    	},
+	    json: true
+	};
+	try{
+		let parsedBody = await rp(options);
+		return parsedBody;
+	}
+	catch(err){
+		safeLog(err);
+		return null;
+	}
+}
+
+
+// strip Basic auth from logging
+function safeLog(obj) {
+	let str = util.inspect(obj);
+	console.log(str.replace(/Basic [+/A-Za-z0-9]+/g,'Basic ******'));
+}
+
